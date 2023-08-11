8075ee9dba51c4b758cab6dd5c77bd1ab037e31f
Asmae Hariti <haritiasmaa74@gmail.com>
2023-08-11 23:03:18 +0100

    fixing versions

---
+++ resources/views/livewire/calendar.blade.php
@@ -0,0 +1,18 @@
+
+<div>
+        <h1>{{ $month }} - {{ $year }}</h1>
+
+
+        {{-- <div class="grid grid-cols-7 gap-1 bg-gray-100 ">
+
+            @foreach ($days as $week )
+              @foreach ($week as $say )
+              <div class="aspect-square flex items-center justify-center">
+                {{ $day }}
+              </div>
+
+              @endforeach
+            @endforeach
+        </div> --}}
+    </div>
+
