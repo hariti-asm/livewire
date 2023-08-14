36e6ee243eea0b3206d87440eaff65bbdf0c9ab1
Asmae Hariti <haritiasmaa74@gmail.com>
2023-08-14 10:21:41 +0100

    days + %

--- resources/views/livewire/calendar.blade.php
+++ resources/views/livewire/calendar.blade.php
@@ -62,7 +62,7 @@
             wire:click="previous">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" aria-hidden="true" focusable="false" class="css-p3qwn0 ejhntxf0"><path fill-rule="evenodd" d="M15.749 20.5c-.24 0-.48-.086-.673-.26l-8.249-7.496a1 1 0 0 1 0-1.479l8.249-7.504a1 1 0 0 1 1.346 1.478l-7.436 6.765 7.436 6.756a1.002 1.002 0 0 1-.673 1.74" ></path></svg>
         </button>
-    <h2 class="text-center font-semibold" >{{ $selectedMonthName }} - {{ $year }}</h2>
+    <h2 class="text-center font-semibold" >{{ $selectedMonthName }}  {{ $year }}</h2>
         <button class="" wire:click="next">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" aria-hidden="true" focusable="false" class="css-p3qwn0 e10xbrur0"><path fill-rule="evenodd" d="M8.25 20.5a1.002 1.002 0 0 1-.673-1.74l7.436-6.756-7.436-6.765a1 1 0 1 1 1.346-1.478l8.249 7.504a1 1 0 0 1 0 1.479L8.923 20.24c-.193.174-.433.26-.673.26"></path></svg>
 
